1941db01fd3a9fe19176b5c04eb9c7ea15bfd2ff
Mohamad-Hachem <mohamad27hachem@outlook.com>
2023-10-12 11:53:57 +0200

    updating website

--- shop/admin.py
+++ shop/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Products, Order
 
 # Register your models here.
+admin.site.site_header = "Mohamad's E-commerce Site"
+admin.site.site_title = "Mohamad's E-commerce"
+admin.site.index_title = "Manage Mohamad's E-commerce "
+
 admin.site.register(Products)
 admin.site.register(Order)
--- shop/views.py
+++ shop/views.py
@@ -13,7 +13,7 @@ def index(request):
         product_objects = product_objects.filter(title__icontains=item_name)
 
     # pagination we want only certain amount of product showing
-    paginator = Paginator(product_objects, 4)
+    paginator = Paginator(product_objects, 8)
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
 
